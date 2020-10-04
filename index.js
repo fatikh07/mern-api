9410a4ce48efa7da0879e058dafbcff8f696acc3
fatikh07 <fatihuddin5@gmail.com>
2020-10-04 07:54:53 +0700

    Tutorial 31

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const authRoutes = require("./src/routes/auth");
@@ -28,4 +29,11 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 });
 
-app.listen(4000);
+mongoose
+  .connect(
+    "mongodb+srv://udin:udin123@cluster0.dlpky.mongodb.net/<dbname>?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(4000, () => console.log("Connection Success"));
+  })
+  .catch((err) => console.log(err));
