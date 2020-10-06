11c084e739b2ebbdc5d5f03dd384325e8e14679a
fatikh07 <fatihuddin5@gmail.com>
2020-10-06 21:22:32 +0700

    Tutorial 37

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -15,6 +15,8 @@ router.post(
   blogController.createBlogPost
 );
 
-router.get('/posts', blogController.getAllBlogPost)
+router.get("/posts", blogController.getAllBlogPost);
+
+router.get("/post/:postId", blogController.getBlogPostById);
 
 module.exports = router;
